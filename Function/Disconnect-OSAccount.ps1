c4943f764c89125630f592d4283d1841f1482242
Augustin Ziegler <augustin.ziegler@gmail.com>
2019-04-07 15:37:58 +0200

    new command: Disconnect-OSAccount

---
+++ Function/Disconnect-OSAccount.ps1
@@ -0,0 +1,48 @@
+﻿<#
+    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .INPUTS
+
+    .OUTPUTS
+
+    .EXAMPLE
+
+    .LINK
+
+    https://developer.openstack.org/api-ref/identity/v3/#revoke-token
+
+    .NOTES
+#>
+function Disconnect-OSAccount
+{
+    param
+    (
+    )
+
+    try 
+    {
+        Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type TRACE -Message 'start'
+ 
+        Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type INFO -Message "remove auth token"
+
+        Invoke-OSApiRequest -HTTPVerb Delete -Type identity -Uri "/auth/tokens" -NoOutput
+
+        $Global:OS_Username = $null
+        $Global:OS_Domain = $null
+        $Global:OS_Project = $null
+        $Global:OS_AuthToken = $null
+        $Global:OS_Endpoints = $null
+        $Global:OS_EndpointInterfaceType = $null
+    }
+    catch 
+    {
+        Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type ERROR -Exception $_
+        throw
+    }
+    finally
+    {
+        Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type TRACE -Message 'end'
+    }
+}
