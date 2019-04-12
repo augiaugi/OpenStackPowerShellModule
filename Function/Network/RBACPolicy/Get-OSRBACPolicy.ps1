ce3479e84813d6d25e5a1bffdc68d66d68f72758
Augustin Ziegler <augustin.ziegler@gmail.com>
2019-04-12 13:08:31 +0200

    new command: Get-OSRBACPolicy, Remove-OSRBACPolicy

---
+++ Function/Network/RBACPolicy/Get-OSRBACPolicy.ps1
@@ -0,0 +1,72 @@
+﻿<#
+    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .PARAMETER ImputObject
+
+    .PARAMETER Name
+
+    .INPUTS
+
+    .OUTPUTS
+
+    .EXAMPLE
+
+    .LINK
+
+        https://developer.openstack.org/api-ref/network/v2/?expanded=#list-rbac-policies
+
+    .NOTES
+#>
+function Get-OSRBACPolicy
+{
+    [CmdLetBinding(DefaultParameterSetName = 'All')]
+    Param
+    (
+        [Parameter (ParameterSetName = 'ImputObject', Mandatory = $true, ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('ID', 'Identity', 'RBACPolicy')]
+        $ImputObject
+    )
+
+    process
+    {
+        try
+        {
+            Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type TRACE -Message "start, ParameterSetName [$($PsCmdlet.ParameterSetName)]"
+
+            switch ($PsCmdlet.ParameterSetName)
+            {
+                'All'
+                {
+                    Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type INFO -Message "get all RBACPolicy"
+                    Write-Output (Invoke-OSApiRequest -Type network -Uri "/v2.0/rbac_policies" -Property 'rbac_policies' -ObjectType 'OS.RBACPolicy')
+                }
+                'ImputObject'
+                {
+                    foreach($ImputObject in $ImputObject)
+                    {
+                        $ImputObject = Get-OSObjectIdentifierer -Object $ImputObject -PropertyHint 'OS.RBACPolicy'
+
+                        Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type INFO -Message "get RBACPolicy [$ImputObject]"
+                        Write-Output (Invoke-OSApiRequest -Type network -Uri "/v2.0/rbac_policies/$ImputObject" -Property 'rbac_policie' -ObjectType 'OS.RBACPolicy')
+                    }
+                }
+                default
+                {
+                    throw "unexpected ParameterSetName [$ParameterSetName]"
+                }
+            }
+        }
+        catch
+        {
+            Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type ERROR -Exception $_
+            throw
+        }
+        finally
+        {
+            Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type TRACE -Message 'end'
+        }
+    }
+}
---
+++ Function/Network/RBACPolicy/Remove-OSRBACPolicy.ps1
@@ -0,0 +1,56 @@
+﻿<#
+    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .PARAMETER ImputObject
+
+    .INPUTS
+
+    .OUTPUTS
+
+    .EXAMPLE
+
+    .LINK
+
+        https://developer.openstack.org/api-ref/network/v2/?expanded=#delete-rbac-policy
+
+    .NOTES
+#>
+function Remove-OSRBACPolicy
+{
+    [CmdLetBinding(DefaultParameterSetName = 'Default')]
+    Param
+    (
+        [Parameter (ParameterSetName = 'Default', Mandatory = $true, ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('ID', 'Identity', 'RBACPolicy')]
+        $ImputObject
+    )
+
+    process
+    {
+        try
+        {
+            Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type TRACE -Message "start"
+
+            foreach($ImputObject in $ImputObject)
+            {
+                $ImputObject = Get-OSObjectIdentifierer -Object $ImputObject -PropertyHint 'OS.RBACPolicy'
+
+                Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type INFO -Message "remove RBACPolicy [$ImputObject]"
+                
+                Invoke-OSApiRequest -HTTPVerb Delete -Type network -Uri "/v2.0/rbac_policies/$ImputObject" -NoOutput
+            }
+        }
+        catch
+        {
+            Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type ERROR -Exception $_
+            throw
+        }
+        finally
+        {
+            Write-OSLogging -Source $MyInvocation.MyCommand.Name -Type TRACE -Message 'end'
+        }
+    }
+}
